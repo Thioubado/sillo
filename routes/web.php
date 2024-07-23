3880ec318f890c850903af291e6b015094705ac8
Momo <seckndanane16@gmail.com>
2024-07-23 23:15:40 +0200

    erreur au niveau de controller

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,14 @@
  */
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
 
 // Routes publiques
-Route::get('/', Welcome::class);
+ Route::get('/welcome', [WelcomeController::class, 'index']);
+
+// Route::get('/', function () {
+//     // event(new Accueil);
+//     return view('welcome');
+
+// });
 
