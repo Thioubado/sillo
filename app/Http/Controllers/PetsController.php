157c5db830132a88019707b4dd7816709bb72ded
Momo <seckndanane16@gmail.com>
2024-08-01 00:11:05 +0200

    error au niveau de la vue

---
+++ app/Http/Controllers/PetsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PetsController extends Controller
+{
+    //
+    public function pets()
+	{
+		// return 'ok2';
+		return 'ok';
+	}
+}
--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,14 @@
  * (ɔ) LARAVEL.Sillo.org - 2015-2024
  */
 
+use App\Http\Controllers\PetsController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 // Routes publiques
 Route::get('/', [WelcomeController::class, 'index'])->name('home)');
+
+Route::get('/pet', [PetsController::class, 'index'])->name('pet)');
 
 // Route::get('/', function () {
 //     // event(new Accueil);
